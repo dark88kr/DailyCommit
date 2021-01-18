9b06d4f71d63be5be387207288b158a3635ba6ad
Yonggi <dark88kr@gmail.com>
2021-01-18 21:53:40 +0900

    210118 - example 1

---
+++ example1.R
@@ -0,0 +1,17 @@
+#책실습하기 
+#R까기2
+#example 1. 20만건 넘는 관찰치 도수분포표 만들기
+
+install.packages("hflights")
+library(hflights)
+library(tidyverse)
+summary(hflights)
+str(hflights)
+
+#목적지 확인 - 목적지는 Dest column 에 있음
+
+dest<-hflights$Dest
+str(dest)
+table(dest)
+
+
