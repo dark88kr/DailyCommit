e00b53ab0b8888f3c3710df4f58ea638510fb758
Yonggi <dark88kr@gmail.com>
2021-03-08 22:13:58 +0900

    210308 7강 종료

--- 7강_따라하며 배우는 데이터과학.R
+++ 7강_따라하며 배우는 데이터과학.R
@@ -165,8 +165,62 @@ summary(hwy_lm2)
 #H0 : 평균 0, H1 : 평균 1, p-value가 유효하므로 HO 기각
 #평균 연비 차이가 다른 Class와 차이가 없을때, -7.92감소로 관측될 확률은  0 에 가깝다.H0 기각
 
-#분산분석 역시 3가지 가정 필요
+#분산분석 역시 3가지 가정 필요 - 해당 분석검정에 대해서는 따로 학습 하겠음
 #1.잔차의 분포가 독립
 #2.잔차의 분산이 동일
-#3.잔차의분포가 전규분포를 따른ㄷ
+#3.잔차의분포가 전규분포를 따른다
 
+#두 변수의 분석 
+#수량형x, 범주형 y - x와 y의 산점도를 그려본다, y변수 그룹별로 x변수를 병렬상자그림 그려본다
+#y값에 따른 x의 분포의 차이가 있는지. 이상치, 표본로그오즈와 x의 산점도에 선형 패턴이 있는지
+#glm함수로 일반화 선형모형 적합
+#plot/gim으로 잔차의 분포를 살펴본다
+
+
+#범주형 변수의 일반화 선형모형 로짓,로지스틱 함수
+#y의 값이 0과 1 이항분포로는 선형모형 불능, 로짓변환시 무한대의 값으로 변경 가능 - link함수 사용
+#최대우도법으로 계산하여 베타값을 구하고 무한대의 선형 추정값을 확률변수로 변환하기 위해 로짓의 역함수 사용
+#이항분포와 로짓링크 함수를 사용한 GLM모형을 로지스틱 회귀모형이라 한다
+
+ch<- read.csv('https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/challenger.csv')
+ch <- tbl_df(ch)
+glimpse(ch)
+ch %>% select(distress_ct) %>% distinct()
+
+#첼린저호 분석, 6개의 o_ring 중 몇개의 링이 실패했는지(distress_ct)
+#x는 온도, y는 distress의 산점도
+
+ch %>% ggplot(aes(temperature,distress_ct)) + geom_point() #산점도
+ch %>% ggplot(aes(factor(distress_ct),temperature)) + geom_boxplot() #병렬상자
+
+
+#glm에서 y가 0과 1 사이 숫자면 y=성공/시도횟수, 성공이 0또는 1이면 1의 값을 성공으로 확인
+#y값이 0-1 숫자형 벡터이면 0은 실패, 1은 성공
+#y값이 2차원 매트릭스 (2개의 컬럼) 일때 첫열을 성공회수, 뒷열은 실패횟수로 인식
+#실패한 o_ring을 성공으로 설정, 0_ring - distress_ct를 실패횟수로 인식
+
+ch_lm <- glm(cbind(distress_ct,o_ring_ct-distress_ct) ~
+               temperature, data = ch, family = "binomial")
+ch_lm
+summary(ch_lm)
+#온도의 계수는 -0.17949는 어떻게 해석? - 온도가 1도 상승하면, 성공비율이 0.179만큼 감소한다.
+
+
+#마지막 줄은 모형의 적합도
+#null deviance 20.706 on 22자유도 - 모형적합하기 전 deviance
+#Residual deviance 9.527 on 21자유도 - 모형적합후 deviance
+#둘 사이가 "충분히" 줄어다면 이 모형은 적합하다고 판단 
+#두 deviance의 차이는  HO 가정아래 카이스퀘어 분포를 따르며 - H0:적합하지 않다, H1:적합 
+1-pchisq(11.2,1) # 두 deviance 차이는 11.2 그리고 자유도는 1인 카이스퀘어 테스트(22-21)
+#p-value는 0.0008179733로 0에 가까우므로 H0기각, 모형이 적합하다고 판단 가능 하다/
+
+
+#적합하다고 판단할수 있는 모형에 30도를 집어 넣으면?
+predict(ch_lm,data.frame(temperature=30))
+#8.82 -0.179*30의 값은 3.432159이다, 이렇게 넣어서 계산하든가-> exp(3.43)/(exp(3.43)+1)
+
+predict(ch_lm, data.frame(temperature=30),type="response")
+#96.87로 성공한다 -> 96.87%로 oring이 파괴된다
+
+
+#7강 종ㄹ
