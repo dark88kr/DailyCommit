7e78e4ada5aad6fb641561eae7e780592a0b91a2
Yonggi <dark88kr@gmail.com>
2021-03-19 23:18:48 +0900

    210319 tidymodels 하나의 모델로 피팅함

--- Tidymodels_연습.R
+++ Tidymodels_연습.R
@@ -35,3 +35,38 @@ lm_fit <- lm_mod %>% fit(width ~ initial_volume * food_regime, data = urchins)
 lm_fit
 
 #오늘은 배운거 노트에 정리하기 210318
+summary(lm_fit) #summary함수가 각 x의 추정치와 기울기를 제공하지만, 결과를 다루기 힘든 모양으로 제공한다
+tidy(lm_fit) #요약결과를 예측가능하고 유용한 형식으로 보여주는 tidy함수 사용 한다
+
+tidy(lm_fit) %>% 
+  dwplot(dot_args = list(size=3,color='blue'),
+         whisker_args = list(color = 'red'),
+         vline = geom_vline(xintercept = 0,colour = 'grey50', linetype=1))
+
+
+new_points <- expand.grid(initial_volume = 20, 
+                          food_regime = c("Initial", "Low", "High"))
+new_points
+
+mean_pred <- predict(lm_fit, new_data = new_points) #predict로 평균값 예측
+mean_pred
+
+
+conf_int_pred <- predict(lm_fit, new_data = new_points,type = "conf_int") #predict함수로 신뢰구간 예측
+conf_int_pred
+
+plot_data <- 
+  new_points %>%             #새로운 데이터에
+  bind_cols(mean_pred) %>%   #평균예측값과
+  bind_cols(conf_int_pred)   #새로운 데이터의 신뢰구간 예측값을 합친다.
+plot_data
+
+ggplot(plot_data, aes(x = food_regime)) + 
+  geom_point(aes(y = .pred)) + 
+  geom_errorbar(aes(ymin = .pred_lower, 
+                    ymax = .pred_upper),
+                width = .2) + 
+  labs(y = "urchin size")
+
+#하나의 모델로 피팅시켜둠
+
